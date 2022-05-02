49ccd6570b3656af4f481b90d75d9c7cf3afa232
kiseta <kiseta@gmail.com>
2022-05-02 12:48:25 -0700

    added random gist name in config.js

--- config.js
+++ config.js
@@ -1,10 +1,15 @@
 // config.js
 
+const gistNameList = ["Super_Awesome_Gist_via_API", "New_Gist_via_API", "", "Hi_Im_API_Posted_Gist", "Code_Snippet_on_GitHub_is_called_Gist", "Another_Gist_posted_by_API"];
+
+const rnd = Math.floor(Math.random() * gistNameList.length);
+console.log(rnd, gistNameList[rnd]);
+
 var config = { 
   baseUrl: "https://api.github.com",
   apiToken: "", // update with your personal API Token
   userAgent:"GitHub API Test example",
-  gistName: "New_Gist_via_API",
+  gistName: gistNameList[rnd],
   gistDescription: "Test Gist created via JavaScript GitHub API test",
   fileContent: "UBC IRP Student QA! Posted on: " + new Date().toLocaleString()
 }
